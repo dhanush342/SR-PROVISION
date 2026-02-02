fc1b8c97798bafad4a5f93e07c532dc6be46c1a5
Naginane Dhanush <e0124073@sriher.edu.in>
2026-02-02 05:45:38 +0000

    in hero section the image it will be shop image

--- src/database/site.ts
+++ src/database/site.ts
@@ -1,9 +1,7 @@
 
 export const heroImage = {
   id: "hero-background",
-  description: "A vibrant market stall with a variety of Indian spices and groceries.",
-  imageUrl: "https://images.unsplash.com/photo-1558458654-a145c979a342?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHw5fHxzcGljZXMlMjBtYXJrZXR8ZW58MHx8fHwxNzY5MjE3MDc4fDA&ixlib=rb-4.1.0&q=80&w=1920",
-  imageHint: "spices market"
+  description: "An aisle in a grocery store with shelves stocked with products.",
+  imageUrl: "https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NDE5ODJ8MHwxfHNlYXJjaHwxfHxncm9jZXJ5JTIwc3RvcmV8ZW58MHx8fHwxNzcyMjgzNTAxfDA&ixlib=rb-4.1.0&q=80&w=1920",
+  imageHint: "grocery store"
 };
-
-    
